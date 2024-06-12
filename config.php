2f2ae7da6d8cd7afb6a7b4e200234b234831a098
Enis Gjini <egjini17@gmail.com>
2024-06-12 23:46:47 +0200

    #

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 return [
     'clientID' => '264182616476-515vn0i0mlr73eeu67b7h7vvm8aol45t.apps.googleusercontent.com',
     'clientSecret' => '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',
-    'redirectUri' => 'http://localhost/HR-EK/',
+    'redirectUri' => 'http://localhost/Human-Resource-Management-System/',
     'db' => [
         'host' => 'localhost',
         'name' => 'hr_ek',
